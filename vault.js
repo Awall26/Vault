6eb984391c8ce7dfd69666dce099930d25a3b9a7
Awall26 <awalter72@gmail.com>
2024-12-05 21:16:00 -0500

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,17 @@
+/**
+ * Step 1. Create a string for the user saying "You have receieved this message because you have been chosen to opan an important vault. Here is the secret combination"
+ * Step 2. create a variable with a math equation that equals 10
+ * Step 3. create a variable with a math equation that eqauls 40
+ * Step 4. create a variable with a math equation that eqauls 39
+ * Step 5. Create an alert that displays the combination
+ */
+// Step 1. Create a string for the user saying "You have receieved this message because you have been chosen to opan an important vault. Here is the secret combination"
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination")
+// Step 2. create a variable with a math equation that equals 10
+const first = 50 / 5;
+// Step 3. create a variable with a math equation that eqauls 40
+const second = 4 * 10;
+// Step 4. create a variable with a math equation that eqauls 39
+const third = 40 - 1;
+// Step 5. Create an alert that displays the combination
+alert("Vault Combination is " + first + "-" + second + "-"  + third + " Quick! Open it before someone else does!");
